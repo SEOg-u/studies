585e153e825b1b2f02046ae38737bbb3d1c10636
SEOg-u <seogyeoul1898@gmail.com>
2022-11-18 14:12:38 +0900

    Update 자료구조::정렬::1. 선택 정렬.c

--- 자료구조/정렬/1. 선택 정렬.c
+++ 자료구조/정렬/1. 선택 정렬.c
@@ -6,21 +6,21 @@ int arr[MAX_SIZE];
 
 void selection_sort(int arr[], int n) {
 	int least, tmp;
-	for (int i = 0; i < n - 1; i++) {//    n-1단계 수행되도록 반복 횟수 설정
+	for (int i = 0; i < n - 1; i++) {// n-1단계 수행되도록 반복 횟수 설정
 		least = i;
 		for (int j = i + 1; j < n; j++) { // 각 단계에 맞는 최솟값의 index 찾기
 			if (arr[least] > arr[j]) {
 				least = j;
 			}
 		}
-		tmp = arr[i]; //                     값 교환(제 자리 위치에 최솟값 넣기)
+		tmp = arr[i]; // 값 교환(제 자리 위치에 최솟값 넣기)
 		arr[i] = arr[least];
 		arr[least] = tmp;
 	}
 }
 
 int main() {
-	srand(time(NULL)); //       씨드 변경 함수
+	srand(time(NULL)); // 씨드 변경 함수
 	int n = MAX_SIZE;
 
 	for (int i = 0; i < n; i++) {
